dff3119b2e358d632f0c31ad0c7bcb86fda16183
7-Dany <ali.ali.ameen245@gmail.com>
2024-06-21 00:01:29 +0300

    add docs.

--- JavaScript/Sorting/selectionSort.js
+++ JavaScript/Sorting/selectionSort.js
@@ -1,5 +1,11 @@
 /*
- * Selection Sort
+   _____      __          __  _                _____            __
+  / ___/___  / /__  _____/ /_(_)___  ____     / ___/____  _____/ /_
+  \__ \/ _ \/ / _ \/ ___/ __/ / __ \/ __ \    \__ \/ __ \/ ___/ __/
+ ___/ /  __/ /  __/ /__/ /_/ / /_/ / / / /   ___/ / /_/ / /  / /_
+/____/\___/_/\___/\___/\__/_/\____/_/ /_/   /____/\____/_/   \__/
+
+ * 
  * It works by:
  *  1. Selecting the smallest (or largest, depending on sorting order) element from the unsorted part of the array.
  *  2. Swapping it with the leftmost unsorted element.
