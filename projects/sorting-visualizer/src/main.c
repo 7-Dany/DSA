77d7b417f0791df6b904511c0a242cf185cf001e
Hetler <https://github.com/H3tler>
2024-06-21 23:51:36 +0300

    It's working

--- projects/sorting-visualizer/src/main.c
+++ projects/sorting-visualizer/src/main.c
@@ -2,6 +2,8 @@
 #include "sorting.h"
 #include <pthread.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
 
 typedef enum {
     INSERTION,
@@ -21,8 +23,12 @@ typedef struct {
     uint32_t delay;
 } Sort_Args;
 
+bool thread_running = false;
+
 void* sort(void *sort_args)
 {
+    thread_running = true;
+
     Sort_Args args = *(Sort_Args*)sort_args;
     int *array = args.arr;
     int len = args.len;
@@ -37,20 +43,24 @@ void* sort(void *sort_args)
         case QUICK: quick_sort(array, len, delay); break;
         case RADIX: radix_sort(array, len, delay); break;
         case SHUFFLE: shuffle_array(array, len, delay); break;
+        // case BOGO: bogo_sort(array, len, delay);
         default: break;// No way I'm allowing bogo sort.
     }
+
+    thread_running = false;
 
     return NULL;
 }
 
 int main(void)
 {
-    const int screenWidth = 800;
-    const int screenHeight= 500;
+    const int screenWidth = 1600;
+    const int screenHeight = 900;
 
-    const int array_size = 50;
+    const int array_size = 100;
     int *array = (int*)malloc(array_size * sizeof(int));
     const int bar_width = screenWidth / array_size;
+    float factor = (screenHeight - 50.f) / (array_size * 10);
 
     for (int i = 0; i < array_size; i++) {
         array[i] = (i + 1) * 10;
@@ -65,28 +75,36 @@ int main(void)
         .type = BUBBLE,
     };
 
+
     while (! WindowShouldClose()) {
         pthread_t thread;
 
-        switch (GetKeyPressed()) {
-            case KEY_S:
-                args.type = SHUFFLE;
-                pthread_create(&thread, NULL, sort, (void*)&args);
-                break;
-            case KEY_B:
-                args.delay = 10;
-                args.type = BUBBLE;
-                pthread_create(&thread, NULL, sort, (void*)&args);
-                break;
-            case KEY_R:
-                args.type = RADIX;
-                args.delay = 10;
-                pthread_create(&thread, NULL, sort, (void*)&args);
-                break;
-            case KEY_M:
-                args.type = MERGE;
-                pthread_create(&thread, NULL, sort, (void*)&args);
-                break;
+        if (! thread_running) {
+            switch (GetKeyPressed()) {
+                case KEY_S:
+                    args.type = SHUFFLE;
+                    pthread_create(&thread, NULL, sort, (void*)&args);
+                    break;
+                case KEY_B:
+                    args.delay = 1;
+                    args.type = BUBBLE;
+                    pthread_create(&thread, NULL, sort, (void*)&args);
+                    break;
+                case KEY_R:
+                    args.type = RADIX;
+                    args.delay = 10;
+                    pthread_create(&thread, NULL, sort, (void*)&args);
+                    break;
+                case KEY_M:
+                    args.type = MERGE;
+                    pthread_create(&thread, NULL, sort, (void*)&args);
+                    break;
+                // case KEY_G:
+                //     args.delay = 1;
+                //     args.type = BOGO;
+                //     pthread_create(&thread, NULL, sort, (void*)&args);
+                //     break;
+            }
         }
 
         ClearBackground(GetColor(0x181818FF));
@@ -95,7 +113,10 @@ int main(void)
         {
             for (int i = 0; i < array_size; i++) {
                 Color color = ColorFromHSV(array[i], 1, 1);
-                DrawRectangle(i * bar_width, screenHeight - array[i], bar_width, array[i], color);
+                float height = array[i] * factor;
+                Vector2 pos = {.x = i * bar_width, .y = screenHeight - height};
+                Vector2 size = {.x = bar_width, .y = height};
+                DrawRectangleV(pos, size, color);
             } 
         }
         EndDrawing();
