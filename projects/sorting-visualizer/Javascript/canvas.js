1ba7d8ba4d0e0438a84725a8437e056aef468cbf
7-Dany <ali.ali.ameen245@gmail.com>
2024-06-30 11:18:17 +0300

    prepared sorting algorithms for visualization

---
+++ projects/sorting-visualizer/Javascript/canvas.js
@@ -0,0 +1,123 @@
+class Canvas {
+    /**
+     * 
+     * @param {string} canvasId specificed canvas id to show sorting visualization.
+     * @param {int[]} array specificed array to visualize its sorting process.
+     */
+    constructor(canvasId, array) {
+        this.canvas = document.querySelector(`#${canvasId}`)
+        this.context = this.canvas.getContext('2d')
+        this.array = array
+        this.maxNumber = this.getMaxElement(this.array)
+        this.createChart(this.array)
+    }
+
+    /**
+     * Create rectangle on canvas, with position (x, y), and dimentions (width, height)
+     * and specific color for the rectangle.
+     * @param {number} x rectangle X position.
+     * @param {number} y rectangle Y position.
+     * @param {number} width rectangle width.
+     * @param {number} height rectangle height.
+     * @param {string} color rectangle color.
+     */
+    createRect = (x, y, width, height, color) => {
+        this.context.fillStyle = `${color}`
+        this.context.fillRect(x, y, width, height)
+    }
+
+    /**
+     * Converts an HSL color value to RGB.
+     * Assumes h, s, and l are contained in the set [0, 1]
+     * and returns r, g, and b in the set [0, 255].
+     * @param {number} h hue for the color.
+     * @param {number} s saturation for the color.
+     * @param {number} l lighting for the color.
+    */
+    hslToRgb = (h, s, l) => {
+        h /= 360;
+        s /= 100;
+        l /= 100;
+
+        let r, g, b;
+
+        if (s === 0) {
+            r = g = b = l; // achromatic
+        } else {
+            const hue2rgb = (p, q, t) => {
+                if (t < 0) t += 1;
+                if (t > 1) t -= 1;
+                if (t < 1 / 6) return p + (q - p) * 6 * t;
+                if (t < 1 / 2) return q;
+                if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+                return p;
+            };
+
+            const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+            const p = 2 * l - q;
+
+            r = hue2rgb(p, q, h + 1 / 3);
+            g = hue2rgb(p, q, h);
+            b = hue2rgb(p, q, h - 1 / 3);
+        }
+
+        const toHex = x => {
+            const hex = Math.round(x * 255).toString(16);
+            return hex.length === 1 ? '0' + hex : hex;
+        };
+
+        return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+    }
+
+
+    /**
+     * Get the maximum number from the array.
+     * @param {int[]} array specified array to get the maximum number from.
+     * @returns {int} returns the maximum number.
+     */
+    getMaxElement = (array) => {
+        let maxNumber = Number.MIN_SAFE_INTEGER
+
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] > maxNumber) maxNumber = array[i]
+        }
+
+        return maxNumber
+    }
+
+    /**
+     * Sleep specified amount of time before continue.
+     * @param {int} ms milliseconds.
+     * @returns {Promise<void>}
+     */
+    sleep = (ms) => {
+        return new Promise(resolve => setTimeout(resolve, ms))
+    }
+
+    /**
+     * Create canvas with rectangles of different shapes and colors to visualize sorting.
+     * @param {int[]} array specified array to visualize on canvas.
+    */
+    createChart = async (array) => {
+        // Delete the previous canvas
+        let canvasWidth = this.canvas.width
+        let canvasHeight = this.canvas.height
+        this.context.clearRect(0, 0, canvasWidth, canvasHeight)
+
+        // Create a new canvas to visualize numbers.
+        let x = 0;
+        let maxElement = this.maxNumber
+        for (let i of array) {
+            let height = (canvasHeight / maxElement) * i - 5
+            let width = canvasWidth / array.length
+            let color = this.hslToRgb((i / maxElement) * 360, 100, 50)
+            this.createRect(x, canvasHeight - height, width, height, color)
+            x += width
+        }
+
+        // Sleep few milliseconds to see the content.
+        await this.sleep(20);
+    }
+}
+
+export default Canvas
---
+++ projects/sorting-visualizer/Javascript/sorts.js
@@ -0,0 +1,274 @@
+class Sort {
+    /**
+     * Contains all sorting algorithms
+     * @param {Function} createChart 
+    */
+    constructor(createChart) {
+        this.createChart = createChart
+    }
+
+    get events() {
+        return {
+            "s": this.shuffleArray,
+            "r": this.radixSort,
+            "i": this.insertionSort,
+            "b": this.bubbleSort,
+            "w": this.selectionSort,
+            "q": this.quickSort,
+        }
+    }
+
+    /**
+     * Shuffle the array
+     * @param {int[]} array array of numbers. 
+     * @returns {int[]} shuffled array.
+    */
+    shuffleArray = async (array) => {
+        for (let i = array.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+
+            [array[i], array[j]] = [array[j], array[i]];
+
+            await this.createChart(array)
+        }
+
+        return array;
+    }
+
+    getDigit = (number, index) => {
+        return Math.floor((number / Math.pow(10, index)) % 10)
+    }
+
+    counting = async (array, index) => {
+        // Count
+        let counts = Array(10).fill(0)
+        for (let i = 0; i < array.length; i++) {
+            let d = this.getDigit(array[i], index)
+            counts[d] += 1
+        }
+
+        // Prefix sum
+        for (let i = 1; i < counts.length; i++) {
+            counts[i] += counts[i - 1]
+        }
+
+        // Create output array 
+        let output = [...array]
+        for (let i = array.length - 1; i >= 0; i--) {
+            let d = this.getDigit(output[i], index)
+            let j = counts[d] - 1
+            counts[d] -= 1
+            array[j] = output[i]
+            await this.createChart(array)
+        }
+
+        return array
+    }
+
+    /**
+     * Radix sort algorithm.
+     * @param {int[]} array 
+     * @returns  {int[]} sorted array
+    */
+    radixSort = async (array) => {
+        // Get the number with maxdigits
+        let maxNumber = Number.MIN_SAFE_INTEGER
+        let minNumber = Number.MAX_SAFE_INTEGER
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] > maxNumber) maxNumber = array[i]
+            if (array[i] < minNumber) minNumber = array[i]
+        }
+
+        // Shift if their is negative number
+        if (minNumber < 0) {
+            for (let i = 0; i < array.length; i++) {
+                array[i] += minNumber * -1
+            }
+        }
+
+        // Sort for each digit
+        let digits = Math.floor(Math.log10(maxNumber)) + 1
+        for (let i = 1; i < digits; i++) {
+            array = await this.counting(array, i)
+        }
+
+        if (minNumber < 0) {
+            for (let i = 0; i < array.length; i++) {
+                array[i] += minNumber
+            }
+        }
+
+        await this.createChart(array)
+        return array
+    }
+
+    partition = async (array, start, end) => {
+        let i = start - 1
+
+        for (let j = start; j < end; j++) {
+            if (array[j] < array[end]) {
+                i++
+                let temp = array[j]
+                array[j] = array[i]
+                array[i] = temp
+
+                await this.createChart(array)
+            }
+        }
+
+        i++
+        let temp = array[i]
+        array[i] = array[end]
+        array[end] = temp
+
+        await this.createChart(array)
+        return i
+    }
+
+    /**
+     * Quick sort algorithm.
+     * @param {int[]} array array to be sorted
+     * @param {int} start start of the array (default = 0)
+     * @param {int} end end of the array (default = array.length - 1)
+     * @returns {int[]} sorted array
+     */
+    quickSort = async (array, start = 0, end = array.length - 1) => {
+        if (start > end) return
+        let pivot = await this.partition(array, start, end);
+        this.quickSort(array, start, pivot - 1)
+        this.quickSort(array, pivot + 1, end)
+    }
+
+    split = (array) => {
+        let right = []
+        let left = []
+        let middle = Math.floor(array.length / 2)
+
+        for (let i = 0; i < array.length; i++) {
+            if (i < middle) {
+                left.push(array[i])
+            } else {
+                right.push(array[i])
+            }
+        }
+
+        return [left, right]
+    }
+
+    merge = async (left, right) => {
+        let i = 0;
+        let j = 0;
+        let results = []
+
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j]) {
+                results.push(left[i])
+                i++
+            } else {
+                results.push(right[j])
+                j++
+            }
+        }
+
+        while (i < left.length) {
+            results.push(left[i])
+            i++
+        }
+
+        while (j < right.length) {
+            results.push(right[j])
+            j++
+        }
+
+        await this.createChart(results)
+        return results
+    }
+
+    /**
+     * Merge sort algorithm
+     * @param {int[]} array array to be sorted
+     * @returns {int[]}
+    */
+    mergeSort = async (array) => {
+        if (array.length === 1) return array
+
+        let [left, right] = this.split(array)
+
+        left = this.mergeSort(left)
+        right = this.mergeSort(right)
+
+        return await this.merge(left, right)
+    }
+
+    /**
+     * Insertion sort algorithm.
+     * @param {int[]} array array to be sorted.
+     * @returns {int[]} sorted array
+    */
+    insertionSort = async (array) => {
+        for (let i = 1; i < array.length; i++) {
+            let temp = array[i]
+            let j = i - 1
+
+            while (j >= 0 && array[j] > temp) {
+                array[j + 1] = array[j]
+                j--
+
+                await this.createChart(array)
+            }
+
+            await this.createChart(array)
+            array[j + 1] = temp
+        }
+
+        await this.createChart(array)
+        return array
+    }
+
+    /**
+     * Bubble sort algorithm.
+     * @param {int[]} array array to be sorted
+     * @returns {int[]} sorted array
+    */
+    bubbleSort = async (array) => {
+        for (let i = 0; i < array.length - 1; i++) {
+            for (let j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    let temp = array[j]
+                    array[j] = array[j + 1]
+                    array[j + 1] = temp
+                    await this.createChart(array)
+                }
+            }
+        }
+
+        await this.createChart(array)
+        return array
+    }
+
+    /**
+     * Selection sort algorithm.
+     * @param {int[]} array array to be sorted
+     * @returns {int[]} sorted array
+    */
+    selectionSort = async (array) => {
+        for (let i = 0; i < array.length; i++) {
+            let min = i
+            for (let j = i; j < array.length; j++) {
+                if (array[j] < array[min]) {
+                    min = j
+                }
+            }
+
+            let temp = array[min]
+            array[min] = array[i]
+            array[i] = temp
+            await this.createChart(array)
+        }
+
+        await this.createChart(array)
+        return array
+    }
+}
+
+export default Sort
