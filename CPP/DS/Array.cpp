8db13fba2f9a7f2e464524b7bcf1f66542483be3
7-Dany <ali.ali.ameen245@gmail.com>
2024-09-20 01:30:35 +0300

    add cpp ds folder with array, linked list implementation

---
+++ CPP/DS/Array.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+
+template <typename T>
+class Array
+{
+private:
+    T *array;
+    int capacity;
+    int length;
+
+    void resize(int new_capacity)
+    {
+        capacity = new_capacity;
+        T *new_array = new T[capacity];
+
+        for (int i = 0; i < length; i++)
+        {
+            new_array[i] = array[i];
+        }
+
+        delete[] array;
+
+        array = new_array;
+    }
+
+public:
+    template <typename U>
+    friend std::ostream &operator<<(std::ostream &os, const Array<T> &arr);
+
+    Array()
+    {
+        capacity = 8;
+        length = 0;
+        array = new T[capacity];
+    }
+
+    Array(std::initializer_list<T> init_list)
+    {
+        length = init_list.size();
+        capacity = length * 2;
+        array = new T[capacity];
+
+        int i = 0;
+        for (T element : init_list)
+        {
+            array[i] = element;
+        }
+    }
+
+    T &operator[](int index)
+    {
+        if (index >= 0 && index < length)
+        {
+            return array[index];
+        }
+
+        throw std::out_of_range("Index is out of bound.");
+    }
+
+    void print() const
+    {
+        for (int i = 0; i < length; i++)
+        {
+            std::cout << array[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    void swap(int x, int y)
+    {
+        if (x >= 0 && x < length && y >= 0 && y < length && x != y)
+        {
+            T temp = array[x];
+            array[x] = array[y];
+            array[y] = temp;
+        }
+    }
+
+    void push(T element)
+    {
+        if (length == capacity)
+        {
+            resize(capacity * 2);
+        }
+
+        array[length] = element;
+        length++;
+    }
+
+    T pop()
+    {
+        if (length == 0)
+        {
+            return nullptr;
+        }
+
+        length--;
+        T element = array[length];
+
+        if (length <= capacity / 4)
+        {
+            resize(capacity / 2);
+        }
+
+        return element;
+    }
+
+    ~Array()
+    {
+        if (array)
+        {
+            delete[] array;
+        }
+    }
+};
+
+template <typename U>
+std::ostream &operator<<(std::ostream &os, const Array<U> &arr)
+{
+    arr.print();
+    return os;
+}
+
+int main()
+{
+    return 0;
+}
---
+++ CPP/DS/LinkedList.cpp
@@ -0,0 +1,206 @@
+#include <iostream>
+
+template <typename T>
+class Node
+{
+public:
+    T data;
+    Node<T> *next;
+    Node<T> *prev;
+
+    Node(T element)
+    {
+        data = element;
+        next = nullptr;
+        prev = nullptr;
+    }
+};
+
+template <typename T>
+class LinkedList
+{
+private:
+    Node<T> *head;
+    Node<T> *tail;
+
+    void remove_helper(Node<T> *node)
+    {
+        if (node->prev)
+        {
+            node->prev->next = node->next;
+        }
+        else
+        {
+            head = node->next;
+        }
+
+        if (node->next)
+        {
+            node->next->prev = node->prev;
+        }
+        else
+        {
+            tail = node->prev;
+        }
+
+        delete node;
+    }
+
+public:
+    template <typename U>
+    friend std::ostream &operator<<(std::ostream &os, const LinkedList<U> &list);
+
+    LinkedList()
+    {
+        head = nullptr;
+        tail = nullptr;
+    }
+
+    LinkedList(std::initializer_list<T> init_list)
+    {
+        head = nullptr;
+        tail = nullptr;
+
+        for (T element : init_list)
+        {
+            insert(element);
+        }
+    }
+
+    void insert(T element)
+    {
+        if (!head)
+        {
+            head = new Node<T>(element);
+            tail = head;
+        }
+        else
+        {
+            Node<T> *temp = new Node<T>(element);
+            temp->prev = tail;
+            tail->next = temp;
+            tail = temp;
+        }
+    }
+
+    T *pop_head()
+    {
+        // Return nullptr if the list is empty
+        if (!head)
+        {
+            return nullptr; 
+        }
+
+        Node<T> *node = head;
+        head = head->next;
+
+        if (head)
+        {
+            head->prev = nullptr;
+        }
+        // If the list becomes empty, update tail
+        else
+        {
+            tail = nullptr; 
+        }
+
+        // Create a copy of the data
+        T *data = new T(node->data); 
+        delete node;
+
+        // Return pointer to the data for checking
+        return data; 
+    }
+
+    T *pop_tail()
+    {
+        // Return nullptr if the list is empty
+        if (!tail)
+        {
+            return nullptr; 
+        }
+
+        Node<T> *temp = tail;
+        tail = tail->prev;
+
+        if (tail)
+        {
+            tail->next = nullptr;
+        }
+        // If the list becomes empty, update head
+        else
+        {
+            head = nullptr; 
+        }
+
+        // Create a copy of the data
+        T *data = new T(temp->data); 
+        delete temp;
+
+        // Return pointer to the data for checking
+        return data; 
+    }
+
+    void remove(T element)
+    {
+        Node<T> *current = head;
+        while (current)
+        {
+            if (current->data == element)
+            {
+                remove_helper(current);
+                return;
+            }
+            current = current->next;
+        }
+    }
+
+    bool search(T element)
+    {
+        Node<T> *current = head;
+        while (current)
+        {
+            if (current->data == element)
+            {
+                return true;
+            }
+            current = current->next;
+        }
+
+        return false;
+    }
+
+    void print() const
+    {
+        Node<T> *current = head;
+        while (current)
+        {
+            std::cout << current->data << " ";
+            current = current->next;
+        }
+        std::cout << std::endl;
+    }
+
+    ~LinkedList()
+    {
+        Node<T> *current = head;
+        while (current)
+        {
+            Node<T> *temp = current->next;
+            delete current;
+            current = temp;
+        }
+    }
+};
+
+template <typename U>
+std::ostream &operator<<(std::ostream &os, const LinkedList<U> &list)
+{
+    list.print();
+    return os;
+}
+
+int main()
+{
+    return 0;
+}
